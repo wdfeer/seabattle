ae265aa53ac31291f4425a33d5ddad8c07c74ca6
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-28 17:12:37 +0200

    Make NextSectors in all directions

--- src/next_sector.gd
+++ src/next_sector.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+@export var direction: Vector2i
+
 func _process(_delta: float) -> void:
 	visible = Waves.wave_state == Waves.WaveState.Finished
 	monitoring = visible
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Boat and body.team == Boat.Team.Player:
-		var pos = Sectors.current + Vector2i.RIGHT
+		var pos = Sectors.current + direction
 		Sectors.sectors.get_or_add(pos, 3)
 		Sectors.current = pos
