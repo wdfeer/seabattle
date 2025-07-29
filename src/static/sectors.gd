72f9e9f76f497ce82cc803afd4f3261ea29093d0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-29 06:49:53 +0200

    Fix player pos set when moving sectors vertically

--- src/static/sectors.gd
+++ src/static/sectors.gd
@@ -10,4 +10,5 @@ static func switch(delta: Vector2i, scene_tree: SceneTree):
 	Sectors.sectors.get_or_add(pos, 1)
 	Sectors.current = pos
 	var player_boat = PlayerController.get_player(scene_tree)
+	delta.y *= -1
 	player_boat.position = Vector2i(720, 720) - delta * 600
