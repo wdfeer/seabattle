c80b94f8b4ac5659419267336ecd02bb7981d660
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-07-30 08:59:58 +0200

    Fix sector vertical position issues

--- src/static/sectors.gd
+++ src/static/sectors.gd
@@ -11,7 +11,6 @@ static func switch(delta: Vector2i, scene_tree: SceneTree):
 	Sectors.current = pos
 	
 	var player_boat = PlayerController.get_player(scene_tree)
-	delta.y *= -1
 	player_boat.position = Vector2i(720, 720) - delta * 600
 	
 	Minimap.instance.update_map()
